8552579925b22079f934c5e94101716d8682bbd5
DE-nagata <nagata@defide-ix.com>
2025-05-28 00:35:40 +0900

    Implement code changes to enhance functionality and improve performance

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
+
 export default function HomePage() {
   return (
     <div>
+      <section className="w-full flex justify-center items-center py-8">
+        <div className="w-full max-w-10xl ">
+          <Image
+            src="/main.png"
+            width={800}
+            height={100}
+            alt="外観と院内の写真が並んだ画像に「当院では、ご来院いただく患者様に 心まで癒されていただけますよう、真心を込めて施術いたします。痛みなどでお悩みの方は、ぜひご相談ください。」と書かれています"
+            className="w-full h-auto rounded-xl shadow-md object-cover"
+          />
+        </div>
+      </section>
       {/* メッセージセクション */}
       <section className="text-center py-10">
         <h2 className="text-3xl font-bold mb-4">
