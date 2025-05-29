82fb311d9a70f908d74045ea680b6e157e356b2a
DE-nagata <nagata@defide-ix.com>
2025-05-29 11:38:24 +0900

    Googleサイトの検証メタタグをhead.tsxに移動し、layout.tsxから削除

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="canonical" href="https://nagata-seikotsuin.org/" />
+      <meta
+        name="google-site-verification"
+        content="sAA--Mwx24gWdOsKGPDAil9RiKf1EmHCza3x0xihFGc"
+      />
+    </>
+  );
+}
